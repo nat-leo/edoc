80f452b6207a5e620924b5c4cceaf274807dc009
natleo <n8wyliu@gmail.com>
2026-02-24 12:38:12 -0800

    change vitest config to grab the setup script vitest.setup.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     environment: "jsdom",
-    setupFiles: "./vitest.setup.ts",
+    globals: true,
+    setupFiles: ["vitest.setup.ts"], // optional
   },
 });
